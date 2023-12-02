0dd215b319265e10d8f24e79e64b12ea8dda330c
bhargavi <bhatgavi31122018@gmail.com>
2023-12-02 17:22:27 +0530

    from scratch

--- build-site.el
+++ build-site.el
@@ -27,7 +27,7 @@
       (list
        (list "org-site:main"
              :recursive t
-             :base-directory "./src"
+             :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
              :with-author nil           ;; Don't include author name
