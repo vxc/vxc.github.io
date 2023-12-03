058c74105464a087ef473ff4722f640dc1892919
bhargavi <bhatgavi31122018@gmail.com>
2023-12-03 20:21:19 +0530

    build script updated

--- build-site.el
+++ build-site.el
@@ -13,30 +13,7 @@
 ;; Install dependencies
 (package-install 'htmlize)
 
-;; Load the publishing system
-(require 'ox-publish)
-(setq org-publish-project-alist
-      '(
-        ;; Project "my-blog"
-        ("content-notes"
-         :base-directory "~/org/org-website/contents"
-         :base-extension "org"
-         :publishing-directory "~/org/org-website/public_html/"
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :auto-preamble t
-         )
 
-        ;; Project "my-notes"
-        ("content-static"
-         :base-directory "~/org/org-website/contents/"
-         :base-extension "css\\|png\\|jpg\\|pdf\\|ogg\\|txt"
-         :publishing-directory "~/org/org-website/public_html/"
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :auto-preamble t
-         )
-        ))
 
 (org-publish-all t)
 
